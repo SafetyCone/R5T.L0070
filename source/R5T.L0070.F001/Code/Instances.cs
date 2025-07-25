7a21eb127d837982a2de3b06876efbf6d7274449
davidcoats <david@getsafetycone.com>
2025-07-25 12:24:48 -0700

    Interim commit.

--- source/R5T.L0070.F001/Code/Instances.cs
+++ source/R5T.L0070.F001/Code/Instances.cs
@@ -7,7 +7,7 @@ namespace R5T.L0070.F001
     {
         public static IDocumentationElementOperator DocumentationElementOperator => F001.DocumentationElementOperator.Instance;
         public static L0069.IDocumentationTargetOperator DocumentationTargetOperator => L0069.DocumentationTargetOperator.Instance;
-        public static L0053.IFileOperator FileOperator => L0053.FileOperator.Instance;
+        public static L0066.IFileOperator FileOperator => L0066.FileOperator.Instance;
         public static IMemberElementOperations MemberElementOperations => F001.MemberElementOperations.Instance;
         public static IMemberElementOperator MemberElementOperator => F001.MemberElementOperator.Instance;
         public static L0053.IXElementOperator XElementOperator => L0053.XElementOperator.Instance;
